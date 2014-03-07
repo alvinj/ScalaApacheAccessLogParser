a33db59a71f452a5e221bdde42d9ae76ffde8ff8
Alvin Alexander <devdaily@gmail.com>
2014-03-06 20:14:03 -0700

    updated documentation

--- src/main/scala/com/alvinalexander/accesslogparser/ApacheCombinedAccessLogRecord.scala
+++ src/main/scala/com/alvinalexander/accesslogparser/ApacheCombinedAccessLogRecord.scala
@@ -4,11 +4,11 @@ package com.alvinalexander.accesslogparser
  * @see http://httpd.apache.org/docs/2.2/logs.html for details
  */
 case class ApacheCombinedAccessLogRecord (
-    clientIpAddress: String,         // may also be `hostname`
+    clientIpAddress: String,         // should be an ip address, but may also be the hostname if hostname-lookups are enabled
     rfc1413ClientIdentity: String,   // typically `-`
     remoteUser: String,              // typically `-`
     dateTime: String,                // [day/month/year:hour:minute:second zone]
-    request: String,                 // `GET ...`
+    request: String,                 // `GET /foo ...`
     serverStatusCode: String,        // 200, 404, etc.
     bytesSent: String,               // may be `-`
     referer: String,                 // where the visitor came from
